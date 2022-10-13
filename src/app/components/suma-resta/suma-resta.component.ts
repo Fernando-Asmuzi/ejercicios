e3190d32d809437b4911ae4b4f21824fa84ce33f
Fernando-Asmuzi <fernandoasmuzi94@gmail.com>
2022-10-13 16:57:44 -0300

    cambios y mejoras en suma-resta

--- src/app/components/suma-resta/suma-resta.component.ts
+++ src/app/components/suma-resta/suma-resta.component.ts
@@ -11,20 +11,30 @@ export class SumaRestaComponent implements OnInit {
   constructor() { }
 
   ejercicios: Sumaresta[] = [];
-  opciones: number[] = [];
+  opciones = [
+    {op_1: 1, op_2: 2, op_3: 3},
+    {op_1: 4, op_2: 5, op_3: 6},
+    {op_1: 7, op_2: 8, op_3: 9},
+    {op_1: 1, op_2: 2, op_3: 3},
+    {op_1: 4, op_2: 5, op_3: 6}
+  ];
   p: number = 1;
+  resultado: number = 0;
+  mensaje: boolean = false;
 
 
   ngOnInit(): void {
       this.generarEjercicios();
 
-      for(let i of this.ejercicios){
+      /* this.formatearResultado(); */
+
+     /*  for(let i of this.ejercicios){
         this.generarOpciones(i.resultado, i.id_ejercicio)
-      }
+      } */
   }
 
   generarAleatorio(){
-      return Math.floor(Math.random() * 10);
+      return Math.floor(Math.random() * (10 - 2) + 2);
   }
 
   generarResultado(primero: number, segundo: number, tipo: string){
@@ -39,21 +49,34 @@ export class SumaRestaComponent implements OnInit {
 
   generarEjercicios(){
     this.ejercicios = [
-      {id_ejercicio:0, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: this.generarResultado(this.ejercicios[0].primer_termino, this.ejercicios[0].segundo_termino, this.ejercicios[0].tipo), tipo: "suma"},
-      {id_ejercicio:1, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: this.generarResultado(this.ejercicios[1].primer_termino, this.ejercicios[1].segundo_termino, this.ejercicios[1].tipo), tipo: "suma"},
-      {id_ejercicio:2, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: this.generarResultado(this.ejercicios[2].primer_termino, this.ejercicios[2].segundo_termino, this.ejercicios[2].tipo), tipo: "suma"},
-      {id_ejercicio:3, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: this.generarResultado(this.ejercicios[3].primer_termino, this.ejercicios[3].segundo_termino, this.ejercicios[3].tipo), tipo: "suma"},
-      {id_ejercicio:4, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: this.generarResultado(this.ejercicios[4].primer_termino, this.ejercicios[4].segundo_termino, this.ejercicios[4].tipo), tipo: "suma"}, 
+      {id_ejercicio:0, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: 0 /* this.generarResultado(this.ejercicios[0].primer_termino, this.ejercicios[0].segundo_termino, this.ejercicios[0].tipo) */, tipo: "suma"},
+      {id_ejercicio:1, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: 0 /* this.generarResultado(this.ejercicios[1].primer_termino, this.ejercicios[1].segundo_termino, this.ejercicios[1].tipo) */, tipo: "suma"},
+      {id_ejercicio:2, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: 0 /* this.generarResultado(this.ejercicios[2].primer_termino, this.ejercicios[2].segundo_termino, this.ejercicios[2].tipo) */, tipo: "suma"},
+      {id_ejercicio:3, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: 0 /* this.generarResultado(this.ejercicios[3].primer_termino, this.ejercicios[3].segundo_termino, this.ejercicios[3].tipo) */, tipo: "suma"},
+      {id_ejercicio:4, primer_termino: this.generarAleatorio(), segundo_termino: this.generarAleatorio(), resultado: 0 /* this.generarResultado(this.ejercicios[4].primer_termino, this.ejercicios[4].segundo_termino, this.ejercicios[4].tipo) */, tipo: "suma"}, 
     ]
   }
 
   generarOpciones(resultado: number, indice: number){
-      let opc: number[] = [0,0,0];
-      /* opc.push(1).[2] */
-      console.log(opc);
-
+    
      this.opciones = [
-
+        
      ]    
+  }
+
+  guardarEleccion(eleccion: number){
+      this.resultado = eleccion;
+      this.mensaje = true;
+  }
+
+  reiniciarResultado(){
+    this.resultado = 0;
+    this.mensaje = false;
+  }
+
+  formatearResultado(){
+    if(this.resultado == 0){
+        this.mensaje = false;
+    }
   }
 }
