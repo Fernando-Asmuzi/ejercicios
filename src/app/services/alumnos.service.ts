c2fd8e383de59a3e43130655907ef32fd0d0a9f7
Fernando-Asmuzi <fernandoasmuzi94@gmail.com>
2022-10-29 22:49:58 -0300

    cambios en los primeros dos ejercicios, base de datos funcionando

---
+++ src/app/services/alumnos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Alumno } from '../models/alumno';
+
+const cudOptions = {
+  headers: new HttpHeaders ({'Content-Type': 'application/json'}),
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AlumnosService {
+
+  private urlBase = environment.url_servicios_base;
+  private apiAlumnos = this.urlBase + '/api/inicio';
+
+  constructor(public http: HttpClient) { }
+
+  getAlumnos(): Observable<any> {
+     return this.http.get<Alumno[]>(this.apiAlumnos, cudOptions)
+  }
+}
---
+++ src/app/services/ejercicios.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AlumnoEjercicio } from '../models/alumno_ejercicio';
+
+
+const cudOptions = {
+  headers: new HttpHeaders ({'Content-Type': 'application/json'}),
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EjerciciosService {
+
+  private urlBase = environment.url_servicios_base;
+  private apiAlumnos = this.urlBase + '/api/inicio';
+
+  constructor(public http: HttpClient) { }
+
+  postAlumnoEjercicio(alu_eje: any): Observable<AlumnoEjercicio> {
+      return this.http.post<AlumnoEjercicio>(this.apiAlumnos, alu_eje)
+  }
+}
